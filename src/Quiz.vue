edb098540be16bb9fa923a8df4f240f4bb0674f1
Jēkabs Leinis <jekabsleinis@gmail.com>
2020-05-05 14:53:45 +0300

    Initial commit

---
+++ src/Quiz.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="quiz" class="container">
+      <div class="row">
+          <div class="col-12">
+              {{msg}}
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+        msg: 'Quiz goes here',
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#quiz {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import Quiz from './Quiz.vue'
+
+new Vue({
+  el: '#quiz',
+  render: h => h(Quiz)
+})
