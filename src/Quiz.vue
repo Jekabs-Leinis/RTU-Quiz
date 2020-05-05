b6bc90fe109aa6e78c3436f6efc0471d7143f629
Jēkabs Leinis <jekabsleinis@gmail.com>
2020-05-05 15:04:01 +0300

    Added global bootstrap

--- src/Quiz.vue
+++ src/Quiz.vue
@@ -1,29 +1,31 @@
 <template>
-  <div id="quiz" class="container">
-      <div class="row">
-          <div class="col-12">
-              {{msg}}
-          </div>
-      </div>
-  </div>
+    <div class="container" id="quiz">
+        <div class="row">
+            <div class="col-12">
+                {{msg}}
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-  data () {
-    return {
-        msg: 'Quiz goes here',
+    import 'bootstrap';
+
+    export default {
+        name: 'app',
+        data() {
+            return {
+                msg: 'Quiz goes here',
+            }
+        }
     }
-  }
-}
 </script>
 
 <style lang="scss">
-#quiz {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #quiz {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import Quiz from './Quiz.vue'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
 
 new Vue({
   el: '#quiz',
   render: h => h(Quiz)
-})
+});
